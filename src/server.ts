a86a4476574cebe5052b66f90432dd93851538b5
Denzelle Walker <dwalker@jetlifetech.com>
2022-04-28 10:31:26 -0500

    Removed User Code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import App from '@/app';
-import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
-import PokemonRoute from './routes/pokemon.route';
+import PokemonRoute from '@routes/pokemon.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new PokemonRoute()]);
+const app = new App([new IndexRoute(), new PokemonRoute()]);
 
 app.listen();
